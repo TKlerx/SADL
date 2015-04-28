332bbe752f858c3d7414c28a267c9a97f951f66f
TKlerx <timok@mail.upb.de>
2015-04-28 09:25:39 +0200

    changed package names to 'sadl'

---
+++ PDTTA-core/src/sadl/detectors/PdttaOneClassSvmDetector.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * This file is part of PDTTA, a library for learning Probabilistic deterministic timed-transition Automata.
+ * Copyright (C) 2013-2015  Timo Klerx
+ * 
+ * PDTTA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * PDTTA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with PDTTA.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package sadl.detectors;
+
+import sadl.constants.ProbabilityAggregationMethod;
+import sadl.constants.ScalingMethod;
+import sadl.detectors.featureCreators.FeatureCreator;
+import sadl.interfaces.TrainableDetector;
+import sadl.oneclassclassifier.LibSvmClassifier;
+
+@Deprecated
+public class PdttaOneClassSvmDetector extends PdttaVectorDetector implements TrainableDetector {
+	/**
+	 * Use the PdttaVectorDetector constructor directly instead
+	 * 
+	 * @param aggType
+	 * @param fc
+	 * @param useProbability
+	 * @param gamma
+	 * @param nu
+	 * @param costs
+	 * @param kernelType
+	 * @param eps
+	 * @param degree
+	 * @param scalingMethod
+	 */
+	public PdttaOneClassSvmDetector(ProbabilityAggregationMethod aggType, FeatureCreator fc, int useProbability, double gamma, double nu, double costs,
+			int kernelType, double eps, int degree, ScalingMethod scalingMethod) {
+		super(aggType, fc, new LibSvmClassifier(useProbability, gamma, nu, costs, kernelType, eps, degree, scalingMethod));
+	}
+
+}
---
+++ PDTTA-core/src/sadl/detectors/PdttaVectorDetector.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * This file is part of PDTTA, a library for learning Probabilistic deterministic timed-transition Automata.
+ * Copyright (C) 2013-2015  Timo Klerx
+ * 
+ * PDTTA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * PDTTA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with PDTTA.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package sadl.detectors;
+
+import gnu.trove.list.TDoubleList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sadl.constants.ProbabilityAggregationMethod;
+import sadl.detectors.featureCreators.FeatureCreator;
+import sadl.interfaces.TrainableDetector;
+import sadl.oneclassclassifier.OneClassClassifier;
+import sadl.structure.TimedSequence;
+
+public class PdttaVectorDetector extends PdttaDetector implements TrainableDetector {
+
+	OneClassClassifier c;
+	FeatureCreator fc;
+
+	public PdttaVectorDetector(ProbabilityAggregationMethod aggType, FeatureCreator fc, OneClassClassifier c) {
+		super(aggType);
+		this.c = c;
+		this.fc = fc;
+	}
+
+	@Override
+	protected boolean decide(TDoubleList eventLikelihoods, TDoubleList timeLikelihoods) {
+		final double[] vector = fc.createFeatures(eventLikelihoods, timeLikelihoods, aggType);
+		return c.isOutlier(vector);
+	}
+
+	@Override
+	public void train(List<TimedSequence> trainingSequences) {
+		final List<double[]> trainingSet = new ArrayList<>(trainingSequences.size());
+		for (final TimedSequence s : trainingSequences) {
+			final TDoubleList eventLikelihoods = computeEventLikelihoods(s);
+			final TDoubleList timeLikelihoods = computeTimeLikelihoods(s);
+			trainingSet.add(fc.createFeatures(eventLikelihoods, timeLikelihoods, aggType));
+		}
+		c.train(trainingSet);
+	}
+
+
+}
