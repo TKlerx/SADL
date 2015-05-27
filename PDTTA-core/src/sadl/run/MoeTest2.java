4720f15d71d3a89cf515fe93602e18722735c833
TKlerx <timok@mail.upb.de>
2015-05-27 17:19:08 +0200

    added moe skeleton for hyperparameter optimization

---
+++ PDTTA-core/src/sadl/run/MoeTest2.java
@@ -0,0 +1,45 @@
+/**
+ * This file is part of SADL, a library for learning all sorts of (timed) automata and performing sequence-based anomaly detection.
+ * Copyright (C) 2013-2015  the original author or authors.
+ *
+ * SADL is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * SADL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with SADL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package sadl.run;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.Policy.Parameters;
+
+public class MoeTest2 {
+
+	public static void main(String[] args) throws ClientProtocolException, IOException {
+		final String postUrl = "http://pc-kbpool-8.cs.upb.de:6543/gp/hyper_opt";// put in your url
+		final HttpClient httpClient = HttpClientBuilder.create().build(); // Use this instead
+		final Parameters p = new Parameters(1, 2);
+		final Gson gson = new Gson();
+		final HttpPost post = new HttpPost(postUrl);
+		final String s = Files.readAllLines(Paths.get("testfile")).get(0);
+		// final StringEntity postingString = new StringEntity(gson.toJson(p));// convert your pojo to json
+		final StringEntity postingString = new StringEntity(s);// convert your pojo to json
+		post.setEntity(postingString);
+		System.out.println(EntityUtils.toString(post.getEntity(), "UTF-8"));
+		post.setHeader("Content-type", "application/json");
+		final HttpResponse response = httpClient.execute(post);
+		final String responseString = EntityUtils.toString(response.getEntity(), "UTF-8");
+		System.out.println(responseString);
+		Parameters p2;
+		p2 = gson.fromJson(responseString, Parameters.class);
+		System.out.println(p2);
+
+		// This is the right query
+		// {"domain_info": {"dim": 2, "domain_bounds": [{"max": 1.0, "min": 0.0},{"max": 0.0, "min": -1.0}]}, "gp_historical_info": {"points_sampled":
+		// [{"value_var": 0.01, "value": 0.1, "point": [0.0,0.0]}, {"value_var": 0.01, "value": 0.2, "point": [1.0,-1.0]}]}, "num_to_sample": 1}
+	}
+
+}
---
+++ PDTTA-core/src/sadl/run/Parameters.java
@@ -0,0 +1,45 @@
+/**
+ * This file is part of SADL, a library for learning all sorts of (timed) automata and performing sequence-based anomaly detection.
+ * Copyright (C) 2013-2015  the original author or authors.
+ *
+ * SADL is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * SADL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with SADL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package sadl.run;
+
+public class Parameters {
+	int a;
+	int b;
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	public Parameters(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+
+	@Override
+	public String toString() {
+		return "Parameters [a=" + a + ", b=" + b + "]";
+	}
+
+}
