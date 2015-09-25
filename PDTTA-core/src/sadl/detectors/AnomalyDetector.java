TKlerx/SADL

commit ec21564c0cae7bd17d6c4eef55a7b6123e11088b

comments for understanding of tauPTA anomaly insertion

PDTTA-core/src/sadl/models/TauPTA.java
@@ -303,7 +303,8 @@ public class TauPTA extends PDTTA {
 
 	public void makeAbnormal(AnomalyInsertionType newAnomalyType) {
 		if (this.anomalyType != AnomalyInsertionType.NONE) {
-			logger.error("A TauPTA can only have one type of anomaly. This one already has AnomalyInsertionType {}, which should be overridden with {}",
+			logger.error(
+					"A TauPTA can only have one type of anomaly. This one already has AnomalyInsertionType {}, which should be overwritten with {}. The overwriting was not done!",
 					this.anomalyType, anomalyType);
 			return;
 		}
@@ -311,13 +312,19 @@ public class TauPTA extends PDTTA {
 		setAnomalyType(newAnomalyType);
 		if (anomalyType == AnomalyInsertionType.TYPE_ONE) {
 			logger.debug("TransitionCount before inserting {} anomalies={}", anomalyType, getTransitionCount());
+			// choose a random state on every height and modify the symbol of an outgoing transition of that state to another random symbol
 			insertPerLevelAnomaly(this::computeTransitionCandicatesType13, this::changeTransitionEvent);
 			logger.debug("TransitionCount after inserting {} anomalies={}", anomalyType, getTransitionCount());
 		} else if (anomalyType == AnomalyInsertionType.TYPE_TWO) {
+			// label the k least probable paths as anomaly (every transition on the path is labeled as abnormal)
 			insertSequentialAnomaly(this::insertAnomaly2);
 		} else if (anomalyType == AnomalyInsertionType.TYPE_THREE) {
+			// choose a random state on every height and modify its time probability drastically (the modification of the time values is only done when sampling
+			// them)
 			insertPerLevelAnomaly(this::computeTransitionCandicatesType13, this::changeTimeProbability);
 		} else if (anomalyType == AnomalyInsertionType.TYPE_FOUR) {
+			// choose the k most probable sequences and modify every time value for every transition on the path slightly (the modification of the time values
+			// is only done when sampling them)
 			insertSequentialAnomaly(this::insertAnomaly4);
 		} else if (anomalyType == AnomalyInsertionType.TYPE_FIVE) {
 			insertPerLevelAnomaly(this::computeTransitionCandicatesType5, this::addFinalStateProbability);
@@ -344,7 +351,7 @@ public class TauPTA extends PDTTA {
 				return f.applyAsInt(s1.toString().compareTo(s2.toString()));
 			}
 		};
-		allSequences.stream().sorted(c).limit(SEQUENTIAL_ANOMALY_K).map(s -> labelWithAnomaly(s, getAnomalyType())).collect(Collectors.toList());
+		allSequences.stream().sorted(c).limit(SEQUENTIAL_ANOMALY_K).map(s -> labelWithAnomaly(s, getAnomalyType()));
 		logger.debug("Transitions.size()={}", transitions.size());
 		// allSequences.sort((t1, t2) -> Double.compare(sequenceProbabilities.get(t1), sequenceProbabilities.get(t2)));
 		// final List<UntimedSequence> abnormalSequences = function.apply(allSequences);

PDTTA-core/src/sadl/run/smac/SmacDataGenerator.java
@@ -28,15 +28,15 @@ import org.apache.commons.lang3.SerializationUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
 import sadl.constants.AnomalyInsertionType;
 import sadl.input.TimedInput;
 import sadl.input.TimedWord;
 import sadl.modellearner.TauPtaLearner;
 import sadl.models.TauPTA;
 import sadl.utils.MasterSeed;
-
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
 
 /**
  * 
@@ -109,7 +109,7 @@ public class SmacDataGenerator implements Serializable {
 					}
 					// PTAs of Type 2 and 4 always produce abnormal sequences
 					// it is possible to sample abnormal and normal sequences with abnormal ptas of the other types (1,3,5).
-					// but I don't know how the distribution is so to be fair, i sample all anomalies the same
+					// but I don't know how the distribution is, so to be fair, i sample all anomalies the same
 					for (int i = 0; i < TEST_SIZE; i++) {
 						if (r.nextDouble() < ANOMALY_PERCENTAGE) {
 							boolean wasAnormal = false;

commit 0419ef22a7270234d56b8b70e58d909d46c474f9

some comments and fixing the bug introduced in the previous commit ec21564c0cae7bd17d6c4eef55a7b6123e11088b

PDTTA-core/src/sadl/detectors/AnomalyDetector.java
@@ -191,6 +191,9 @@ public abstract class AnomalyDetector {
 		return result;
 	}
 
+	/**
+	 * Computes the product of the probabilities in log space. @param probabilities the probabilities @return the product of the probabilities in log space
+	 */
 	public static double aggregate(TDoubleList probabilities) {
 		return aggregate(probabilities, ProbabilityAggregationMethod.NORMALIZED_MULTIPLY);
 	}

PDTTA-core/src/sadl/models/TauPTA.java
@@ -352,7 +352,7 @@ public class TauPTA extends PDTTA {
 				return f.applyAsInt(s1.toString().compareTo(s2.toString()));
 			}
 		};
-		allSequences.stream().sorted(c).limit(SEQUENTIAL_ANOMALY_K).map(s -> labelWithAnomaly(s, getAnomalyType()));
+		allSequences.stream().sorted(c).limit(SEQUENTIAL_ANOMALY_K).map(s -> labelWithAnomaly(s, getAnomalyType())).collect(Collectors.toList());
 		logger.debug("Transitions.size()={}", transitions.size());
 		// allSequences.sort((t1, t2) -> Double.compare(sequenceProbabilities.get(t1), sequenceProbabilities.get(t2)));
 		// final List<UntimedSequence> abnormalSequences = function.apply(allSequences);
@@ -407,6 +407,7 @@ public class TauPTA extends PDTTA {
 			probabilities.add(probability);
 			currentState = t.getToState();
 		}
+		probabilities.add(getFinalStateProbability(currentState));
 		return AnomalyDetector.aggregate(probabilities);
 		// return product(probabilities);
 	}
@@ -510,6 +511,8 @@ public class TauPTA extends PDTTA {
 			logger.warn("Chose states on height {} which are leaf states. Inserting a anomalies is not possible.", height);
 		}
 		if (result.size() == 1) {
+			// return an empty list if there is only one transition that is leading to the next level in the tree
+			// there must always be a normal path, because o/w a path from this height on is always abnormal
 			return Collections.emptyList();
 		}
 		return result;
@@ -519,15 +522,15 @@ public class TauPTA extends PDTTA {
 		final TIntSet currentStates = new TIntHashSet(possibleTransitions.stream().mapToInt(t -> t.getFromState()).distinct().toArray());
 		while (currentStates.size() > 0) {
 			final Transition chosenTransition = chooseRandomObject(possibleTransitions, r);
-			final int chosenState = chosenTransition.getFromState();
-			final List<Transition> stateTransitions = possibleTransitions.stream().filter(t -> t.getFromState() == chosenState).collect(Collectors.toList());
+			final int chosenFromState = chosenTransition.getFromState();
+			final List<Transition> stateTransitions = possibleTransitions.stream().filter(t -> t.getFromState() == chosenFromState).collect(Collectors.toList());
 			final List<String> notOccuringEvents = new ArrayList<>(Arrays.asList(alphabet.getSymbols()));
 			for (final Transition t : stateTransitions) {
 				notOccuringEvents.remove(t.getSymbol());
 			}
 			if (notOccuringEvents.size() == 0 || stateTransitions.size() == 0) {
-				logger.warn("Not possible to change an event in state {}", chosenState);
-				currentStates.remove(chosenState);
+				logger.warn("Not possible to change an event in state {}", chosenFromState);
+				currentStates.remove(chosenFromState);
 				continue;
 			} else {
 				final String chosenEvent = notOccuringEvents.get(r.nextInt(notOccuringEvents.size()));