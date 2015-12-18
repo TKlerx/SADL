768c2ad21b41f430a334b376cabc5ab0c3eaa373
TKlerx <timok@mail.upb.de>
2015-12-18 12:31:01 +0100

    Added test for butla preprocessing and pdtta learning

--- PDTTA-core/test/sadl/detectors/PdttaWithPreprocessingTest.java
+++ PDTTA-core/test/sadl/detectors/PdttaWithPreprocessingTest.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 import org.apache.commons.math3.util.Pair;
 import org.junit.Test;
@@ -20,6 +21,7 @@ import sadl.input.TimedInput;
 import sadl.modellearner.ButlaPdtaLearner;
 import sadl.modellearner.PdttaLearner;
 import sadl.models.PDTTA;
+import sadl.models.pta.Event;
 import sadl.oneclassclassifier.ThresholdClassifier;
 import sadl.utils.IoUtils;
 
@@ -38,16 +40,18 @@ public class PdttaWithPreprocessingTest {
 			final Path p = Paths.get(this.getClass().getResource("/pdtta/smac_mix_type1.txt").toURI());
 			final Pair<TimedInput, TimedInput> trainTest = IoUtils.readTrainTestFile(p);
 
-			final ButlaPdtaLearner butla = new ButlaPdtaLearner(10000, 0.05, null, 0.05, 0.05, null, EventsCreationStrategy.SplitEvents,
-					KDEFormelVariant.OriginalButlaVariableBandwidth);
-			final TimedInput splittedTrainSet = butla.splitEventsInTimedSequences(trainTest.getFirst());
+			final ButlaPdtaLearner butla = new ButlaPdtaLearner(20000, EventsCreationStrategy.SplitEvents, KDEFormelVariant.OriginalKDE);
+			// final ButlaPdtaLearner butla = new ButlaPdtaLearner(EventsCreationStrategy.SplitEvents, KDEFormelVariant.OriginalKDE);
+
+			final Pair<TimedInput, Map<String, Event>> pair = butla.splitEventsInTimedSequences(trainTest.getFirst());
+			final TimedInput splittedTrainSet = pair.getFirst();
+			final Map<String, Event> eventMapping = pair.getSecond();
 			final PDTTA model = learner.train(splittedTrainSet);
 			detector.setModel(model);
 			final AnomalyDetection detection = new AnomalyDetection(detector, model);
-			final TimedInput testSet = trainTest.getValue();
-			// TODO method to apply the event preprocessing to a test set with the splits from the train set
+			final TimedInput testSet = butla.getSplitInputForMapping(trainTest.getValue(), eventMapping);
 			final ExperimentResult result = detection.test(testSet);
-			final ExperimentResult expected = new ExperimentResult(0, 0, 0, 0);
+			final ExperimentResult expected = new ExperimentResult(467, 4440, 93, 0);
 			assertEquals(expected, result);
 		} else {
 			System.out.println("Did not do any test because OS is not linux and treba cannot be loaded.");
