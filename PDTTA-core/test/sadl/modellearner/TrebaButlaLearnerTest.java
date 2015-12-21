7cb28a00d4270ff27d5f9dd169cb5d7b80dc52d8
TKlerx <timok@mail.upb.de>
2015-12-21 20:15:48 +0100

    Added t0 parameter for comparing treba and butla

--- PDTTA-core/test/sadl/modellearner/TrebaButlaLearnerTest.java
+++ PDTTA-core/test/sadl/modellearner/TrebaButlaLearnerTest.java
@@ -10,6 +10,7 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import sadl.constants.EventsCreationStrategy;
 import sadl.constants.KDEFormelVariant;
+import sadl.constants.MergeTest;
 import sadl.constants.PTAOrdering;
 import sadl.constants.TransitionsType;
 import sadl.input.TimedInput;
@@ -24,7 +25,7 @@ public class TrebaButlaLearnerTest {
 		// TODO why do they give different results?
 		final ButlaPdtaLearner butla = new ButlaPdtaLearner(100000000, 0.05, TransitionsType.Outgoing, 0.05, 0.05, PTAOrdering.TopDown,
 				EventsCreationStrategy.DontSplitEvents, KDEFormelVariant.OriginalKDE);
-		final TrebaPdfaLearner treba = new TrebaPdfaLearner(0.05, true);
+		final TrebaPdfaLearner treba = new TrebaPdfaLearner(0.05, true, MergeTest.ALERGIA, 0.0, 0);
 		final Path p = Paths.get(this.getClass().getResource("/pdtta/smac_mix_type1.txt").toURI());
 		Pair<TimedInput, TimedInput> input = IoUtils.readTrainTestFile(p);
 
