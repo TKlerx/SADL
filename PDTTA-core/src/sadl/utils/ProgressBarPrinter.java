9d1c2ba9f4b36117f4abb6a35d75e74babbbf1d4
fwitter <fwitter@mail.upb.de>
2016-12-18 01:52:31 +0100

    Moved classes to util package

--- PDTTA-core/src/sadl/utils/ProgressBarPrinter.java
+++ PDTTA-core/src/sadl/utils/ProgressBarPrinter.java
@@ -8,7 +8,7 @@
  *
  * You should have received a copy of the GNU General Public License along with SADL.  If not, see <http://www.gnu.org/licenses/>.
  */
-package sadl.modellearner.rtiplus;
+package sadl.utils;
 
 /**
  * 
